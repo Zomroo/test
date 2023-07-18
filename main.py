05a47f32985d9649612e7dc5ee6eb369d87fe389
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-07-18 21:58:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pyrogram
+import concurrent.futures
 
 bot = pyrogram.Client(
     "my_bot",
@@ -7,12 +8,17 @@ bot = pyrogram.Client(
     bot_token="6377609542:AAF004ZVuKIC3LREINCBhByXJ7gdP6AbTe8"
 )
 
+# Configure concurrent futures thread pool
+executor = concurrent.futures.ThreadPoolExecutor()
+
 @bot.on_message()
 async def forward_message(client, message):
     owner_id = 5500572462
     message_ids = [message.id]
     from_chat_id = message.chat.id
-    await client.forward_messages(owner_id, from_chat_id, message_ids)
+    
+    # Submit message forwarding task to the thread pool for parallel execution
+    await bot.loop.run_in_executor(executor, client.forward_messages, owner_id, from_chat_id, message_ids)
 
 if __name__ == "__main__":
     bot.run()
