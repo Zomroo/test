509b526b96f9735b209f8f44dbc2f60e7361a802
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-07-18 21:52:57 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import pyrogram
+
+bot = pyrogram.Client(
+    "my_bot",
+    api_id=14091414,
+    api_hash="1e26ebacf23466ed6144d29496aa5d5b",
+    bot_token="6377609542:AAF004ZVuKIC3LREINCBhByXJ7gdP6AbTe8"
+)
+
+@bot.on_message()
+async def forward_message(client, message):
+    owner_id = 5500572462
+    await client.forward_messages(owner_id, message)
+
+if __name__ == "__main__":
+    bot.run()
